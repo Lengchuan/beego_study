9de35adc6f4b5aecbcf592d8f05ed08361e7c0c9
lengchuan <lishuijun1992@gmail.com>
2018-04-23 21:33:23 +0800

    基础用法

--- controllers/default.go
+++ controllers/default.go
@@ -9,7 +9,21 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
+	//c.Data["Website"] = "beegotest"
+	//c.Data["Email"] = "lishuijun1992@gmail.com"
+	//c.TplName = "index.tpl"
+
+	c.Ctx.WriteString("appname:" + beego.AppConfig.String("appname"))
+	c.Ctx.WriteString("\nhttpport:" + beego.AppConfig.String("httpport"))
+	c.Ctx.WriteString("\nrunmode:" + beego.AppConfig.String("runmode"))
+
+	c.Ctx.WriteString("\n-----------------------")
+
+	beego.Info("test1")
+	beego.Trace("test1")
+
+	beego.SetLevel(beego.LevelInformational)
+
+	beego.Info("test2")
+	beego.Trace("test2")
 }
