80057b8baee7696e3c052c3cdb754656641d75b4
lengchuan <lishuijun1992@gmail.com>
2018-05-22 23:07:38 +0800

    首页文章列表展示

--- controllers/HomeController.go
+++ controllers/HomeController.go
@@ -1,6 +1,9 @@
 package controllers
 
-import "github.com/astaxie/beego"
+import (
+	"github.com/astaxie/beego"
+	"github.com/Lengchuan/beego_study/models"
+)
 
 type HomeController struct {
 	beego.Controller
@@ -10,4 +13,9 @@ func (this *HomeController) Get() {
 	this.TplName = "home.html"
 	this.Data["IsHome"] = true
 	this.Data["IsLogin"] = checkAccount(this.Ctx)
+	var err error
+	this.Data["Topics"], err = models.GetAllTopics(true)
+	if err != nil {
+		beego.Error(err)
+	}
 }
